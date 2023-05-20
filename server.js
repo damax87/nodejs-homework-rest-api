4ba578b90ed4d16373731b4c50e44495d84ddecf
damax87 <migunoff87@gmail.com>
2023-05-20 16:05:29 +0300

    3 commit: move DB_HOST to process.env

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const app = require('./app')
 
-const DB_HOST = "mongodb+srv://Maxim:kLOWbHhQ3lu2B6Mh@cluster0.we10lyn.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const {DB_HOST} = process.env;
+
 mongoose.connect(DB_HOST)
   .then(() => {
     console.log("Database connection successful");
